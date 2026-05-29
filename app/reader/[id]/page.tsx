185735337cf4301d1c64c858eea4900f43a80084
Barbara Campos <bjcg2022@gmail.com>
2026-05-29 16:45:12 +0100

    fix: reset total da rota de leitor

--- app/reader/[id]/page.tsx
+++ app/reader/[id]/page.tsx
@@ -1,9 +1,9 @@
-import React from 'react'
+import React from "react";
 
 export default function ReaderPage({ params }: { params: { id: string } }) {
   return (
-    <div className="p-8">
-      <h1>Lendo item: {params.id}</h1>
+    <div className="p-6">
+      <h1>Conteúdo do ID: {params.id}</h1>
     </div>
-  )
+  );
 }
