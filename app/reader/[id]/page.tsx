f7695587ad53ce12c6bc0d438de72aef98c7366c
Barbara Campos <bjcg2022@gmail.com>
2026-05-29 16:53:58 +0100

    fix: renomear rota de reader para leitura para resetar cache do sistema de arquivos

--- app/reader/[id]/page.tsx
+++
@@ -1,9 +0,0 @@
-import React from "react";
-
-export default function ReaderPage({ params }: { params: { id: string } }) {
-  return (
-    <div className="p-6">
-      <h1>Conteúdo do ID: {params.id}</h1>
-    </div>
-  );
-}
